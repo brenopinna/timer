784b2c052ee0273b3440df956c4cbb3af248a3d7
brenopinna <brenopinna1008@gmail.com>
2022-08-27 18:58:19 -0300

    correcao logica do timer

--- js/script.js
+++ js/script.js
@@ -7,41 +7,25 @@ const stopButton = document.getElementById('stop');
 
 startButton.addEventListener('click', () => {
    treatInputs(secInput, minInput, hourInput);
-   
-   if(hourInput.value == 0 && minInput.value == 0 && secInput.value == 0) {
+
+   if(hourInput.value == minInput.value == secInput.value == 0) {
       alert("Insert some time!")
    }else{
       disableInputs([secInput, minInput, hourInput]);
       changeButtons(startButton, stopButton);
 
+      let timeLeft = (parseInt(secInput.value) + parseInt(minInput.value * 60) + parseInt(hourInput.value * 60 * 60));
+
       const timer = setInterval(() => {
-         let seconds = parseInt(secInput.value);
-         let minutes = parseInt(minInput.value);
-         let hours = parseInt(hourInput.value);
-
-         let remainsMinutes = minutes > 0;
-         let remainsHours = hours > 0;
-         let remainsSeconds = seconds > 0;
-         let remainsTime = (remainsSeconds || remainsMinutes || remainsHours);
-
+         timeLeft--;
+         const remainsTime = timeLeft > 0;
          if(remainsTime){
-            if(seconds == 0){
-               if(minutes == 0){
-                  if(remainsHours){
-                     hours--;
-                     minutes = 59;
-                  }
-               }
-               seconds = 59;
-               if(remainsMinutes){
-                  minutes--;
-               }
-            }else{
-               seconds--;
-            }
-            setTime(secInput, seconds);
-            setTime(minInput, minutes);
+            const hours = Math.floor(timeLeft / 3600);
+            const minutes = Math.floor((timeLeft / 60) % 60);
+            const seconds = Math.floor(timeLeft % 60);
             setTime(hourInput, hours);
+            setTime(minInput, minutes);
+            setTime(secInput, seconds);
          }else{
             resetTimer();
          }
@@ -70,7 +54,7 @@ function disableInputs([...inputs], disable=true){
 
 function treatInputs(...inputs){
    for(let input of inputs){
-      if(input.value == 0){
+      if(input.value <= 0){
          input.value = '00';
       }else{
          setTime(input, input.value);
